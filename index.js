0c70e5c6f3095a469124be0840c69a1b4820c656
nixenze <thanin.srithai@gmail.com>
2018-12-01 19:50:08 +0700

    add header and title

--- index.js
+++ index.js
@@ -3,15 +3,10 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import CameraScreen from './screens/CameraScreen';
-import TestCameraScreen from './screens/TestCameraScreen';
-import {createBottomTabNavigator,createStackNavigator,createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation';
 
-const Navi = createBottomTabNavigator({
-    Home : {screen: App},
-    Camera : {screen: CameraScreen},
-    Camera2 : {screen: TestCameraScreen},
-    },
-);
+const EaseEat = createStackNavigator({
+    Home: App
+})
 
-AppRegistry.registerComponent(appName, () => Navi);
+AppRegistry.registerComponent(appName, () => EaseEat);
