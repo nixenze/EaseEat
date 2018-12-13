d4e30d5eead1248a3a53ab88976389abe97789f6
test user <testuser@tests-MacBook-Pro.local>
2018-12-13 15:34:15 +0700

    test flatlist

--- screens/FoodListScreen.js
+++ screens/FoodListScreen.js
@@ -7,7 +7,8 @@ import {
     Image,
     Button,
     TouchableNativeFeedback as TouchNative,
-    Platform
+    Platform,
+    FlatList
 } from 'react-native'
 import { createStackNavigator } from 'react-navigation'
 import FoodInfoScreen from './FoodInfoScreen';
@@ -22,50 +23,80 @@ class FoodListScreen extends Component {
     static navigationOptions = {
         title: 'EaseEat',
     };
+    constructor(props) {
+        super(props);
+        this.state = {
+            data :[{
+                engName: 'Hamburger1',
+                thaiName: 'แฮมเบอร์เกอร์',
+                img: require('../images/img0.jpg')
+            }, {
+                engName: 'Hamburger2',
+                thaiName: 'แฮมเบอร์เกอร์',
+                img: require('../images/img0.jpg')
+            }, {
+                engName: 'Hamburger3',
+                thaiName: 'แฮมเบอร์เกอร์',
+                img: require('../images/img0.jpg')
+            },
+        ],
+        };
+    }
+
 
     render() {
         return (
+            <FoodScrollView data={this.state.data}
+            nav={this.props.navigation}
+            />
+        )
+    }
+}
 
-            <ScrollView style={{backgroundColor:'white'}}>
-                <FoodItem />
-                <FoodItem />
-                <FoodItem />
-                <FoodItem />
-                <FoodItem />
-                <FoodItem />
-                <FoodItem />
-                <FoodItem />
-                <FoodItem />
-            </ScrollView>
+class FoodScrollView extends Component {
+    render() {
+        return (
+
+            <FlatList 
+                data={this.props.data}
+                renderItem={(data) => {
+                    <FoodItem
+                    engName={data.engName}
+                    thaiName={data.thaiName}
+                    img={data.img}
+                    />
+                }}
+            />
+        
 
         )
     }
 }
 
-
 class FoodItem extends Component {
+
 
     render() {
         return (
             <TouchNative
-                onPress={() => {}}
+                onPress={() => { }}
                 background={Platform.OS === 'android' ? TouchNative.SelectableBackground() : ''}
             >
-            <View>
-                <View style={foodStyles.container}>
-                    <Image
-                        style={{ width: 150, height: 150 }}
-                        source={require('../images/img0.jpg')}
-                    />
-                    <View style={{paddingLeft:32,justifyContent:'center'}}>
-                    <Text style={{fontSize:20,fontWeight:'bold'}}>Hamburger</Text>
-                    <Text>แฮมเบอร์เกอร์</Text>
-                    </View>
-                    
+                <View style={{ flex: 1 }}>
+                    <View style={foodStyles.container}>
+                        <Image
+                            style={{ width: 150, height: 150 }}
+                            source={this.props.img}
+                        />
+                        <View style={{ paddingLeft: 32, justifyContent: 'center' }}>
+                            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{this.props.engName}</Text>
+                            <Text>{this.props.thaiName}</Text>
+                        </View>
 
+
+                    </View>
+                    <View style={{ height: 1, backgroundColor: 'gainsboro' }} />
                 </View>
-                <View style={{height:1, backgroundColor:'gainsboro'}}/>
-            </View>
             </TouchNative>
         )
     }
@@ -82,7 +113,7 @@ export default createStackNavigator({
 const foodStyles = StyleSheet.create({
     container: {
         height: 150,
-        flexDirection:'row'
+        flexDirection: 'row'
 
     }
 });
